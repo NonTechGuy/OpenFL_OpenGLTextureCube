3b3fa694ca24a7b9b9f7ffcaddc9090514f3aa2a
jlongi <jlongi@gmail.com>
2013-12-26 21:18:23 -0600

    Add to the project.xml tha flags to enable depth and stencil tests. Now works in mobile, desktop and js.

--- Source/Main.hx
+++ Source/Main.hx
@@ -12,8 +12,8 @@ import openfl.gl.GLProgram;
 import openfl.gl.GLTexture;
 import openfl.gl.GLUniformLocation;
 import openfl.utils.Float32Array;
-import openfl.utils.Int16Array;
 import openfl.utils.UInt8Array;
+import openfl.utils.Int16Array;
 import openfl.Assets;
 
 /**
@@ -29,7 +29,7 @@ class Main extends Sprite {
   private var view:OpenGLView;
   private var vertexPositionAttribute:Int;
 
-  private var textureCoordAttribute:Int;
+  private var texCoordAttribute:Int;
   private var texture:GLTexture;
 
   private var cubeVertexPositionBuffer:GLBuffer;
@@ -43,42 +43,39 @@ class Main extends Sprite {
   private var xRot:Float;
   private var yRot:Float;
   private var zRot:Float;
-  private var lastTime:Float;
+  private var lastTime:Float; 
 
   /**
    *
    */
-  public function new() {
+  public function new () {
     super();
 
-    if (OpenGLView.isSupported) {
+    if(OpenGLView.isSupported) {
       view = new OpenGLView();
 
       GL.enable(GL.DEPTH_TEST);
-      GL.depthFunc( GL.LEQUAL );
-      GL.depthMask( true );
 
+      mvMatrix = new Matrix3D();
       mvMatrixStack = [];
       xRot = 0.0;
       yRot = 0.0;
       zRot = 0.0;
       lastTime = 0.0;
 
-      mvMatrix = new Matrix3D();
-
       initializeShaders();
-      initTexture();
       createBuffers();
+      initTexture();
 
       view.render = renderView;
       addChild(view);
-		}
-	}
-	
+    }
+  }
+
   /**
    *
    */
-  private function initializeShaders():Void {
+  private function initializeShaders ():Void {
     var vertexShaderSource = Assets.getText("assets/shader.vert");
 
     var vertexShader = GL.createShader(GL.VERTEX_SHADER);
@@ -88,7 +85,7 @@ class Main extends Sprite {
     if (GL.getShaderParameter(vertexShader, GL.COMPILE_STATUS) == 0) {
       throw "Error compiling vertex shader";
     }
-
+    
     #if desktop
       var fragmentShaderSource = "";
     #else
@@ -101,7 +98,7 @@ class Main extends Sprite {
     GL.shaderSource(fragmentShader, fragmentShaderSource);
     GL.compileShader(fragmentShader);
 
-    if (GL.getShaderParameter (fragmentShader, GL.COMPILE_STATUS) == 0) {
+    if (GL.getShaderParameter(fragmentShader, GL.COMPILE_STATUS) == 0) {
       throw "Error compiling fragment shader";
     }
 
@@ -114,12 +111,11 @@ class Main extends Sprite {
       throw "Unable to initialize the shader program.";
     }
 
-    //
-    vertexPositionAttribute = GL.getAttribLocation(shaderProgram, "aVertexPosition");
+    vertexPositionAttribute = GL.getAttribLocation (shaderProgram, "aVertexPosition");
     GL.enableVertexAttribArray(vertexPositionAttribute);
 
-    textureCoordAttribute = GL.getAttribLocation(shaderProgram, "aTextureCoord");
-    GL.enableVertexAttribArray(textureCoordAttribute);
+    texCoordAttribute = GL.getAttribLocation (shaderProgram, "aTextureCoord");
+    GL.enableVertexAttribArray(texCoordAttribute);
 
     pMatrixUniform  = GL.getUniformLocation(shaderProgram, "uPMatrix");
     mvMatrixUniform = GL.getUniformLocation(shaderProgram, "uMVMatrix");
@@ -129,19 +125,19 @@ class Main extends Sprite {
   /**
    *
    */
-  private function initTexture() {
-    var bitmapData = Assets.getBitmapData("assets/openfl.png");
-    
+  private function initTexture():Void {
     texture = GL.createTexture();
+    var bitmapData = Assets.getBitmapData("assets/openfl.png");
     GL.bindTexture(GL.TEXTURE_2D, texture);
 
     #if js
-      var data = rearrangeColorComponents( bitmapData.getPixels(bitmapData.rect).byteView, bitmapData.width, bitmapData.height );
+      var data = rearrangeColorComponents(bitmapData.getPixels(bitmapData.rect).byteView, bitmapData.width, bitmapData.height);
     #else
       var data = new UInt8Array(bitmapData.getPixels (bitmapData.rect));
     #end
 
     GL.texImage2D(GL.TEXTURE_2D, 0, GL.RGBA, bitmapData.width, bitmapData.height, 0, GL.RGBA, GL.UNSIGNED_BYTE, data);
+
     GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.LINEAR);
     GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.LINEAR);
     GL.bindTexture(GL.TEXTURE_2D, null);
@@ -150,37 +146,37 @@ class Main extends Sprite {
   /**
    *
    */
-   private function createBuffers():Void {
+  private function createBuffers():Void {
     var vertices = [
       // Front face
       -1.0, -1.0,  1.0,
-       1.0, -1.0,  1.0,
-       1.0,  1.0,  1.0,
+      1.0, -1.0,  1.0,
+      1.0,  1.0,  1.0,
       -1.0,  1.0,  1.0,
 
       // Back face
       -1.0, -1.0, -1.0,
       -1.0,  1.0, -1.0,
-       1.0,  1.0, -1.0,
-       1.0, -1.0, -1.0,
+      1.0,  1.0, -1.0,
+      1.0, -1.0, -1.0,
 
       // Top face
       -1.0,  1.0, -1.0,
       -1.0,  1.0,  1.0,
-       1.0,  1.0,  1.0,
-       1.0,  1.0, -1.0,
+      1.0,  1.0,  1.0,
+      1.0,  1.0, -1.0,
 
       // Bottom face
       -1.0, -1.0, -1.0,
-       1.0, -1.0, -1.0,
-       1.0, -1.0,  1.0,
+      1.0, -1.0, -1.0,
+      1.0, -1.0,  1.0,
       -1.0, -1.0,  1.0,
 
       // Right face
-       1.0, -1.0, -1.0,
-       1.0,  1.0, -1.0,
-       1.0,  1.0,  1.0,
-       1.0, -1.0,  1.0,
+      1.0, -1.0, -1.0,
+      1.0,  1.0, -1.0,
+      1.0,  1.0,  1.0,
+      1.0, -1.0,  1.0,
 
       // Left face
       -1.0, -1.0, -1.0,
@@ -190,77 +186,76 @@ class Main extends Sprite {
     ];
     cubeVertexPositionBuffer = GL.createBuffer();
     GL.bindBuffer(GL.ARRAY_BUFFER, cubeVertexPositionBuffer);
-    GL.bufferData(GL.ARRAY_BUFFER, new Float32Array(vertices), GL.STATIC_DRAW);
+    GL.bufferData(GL.ARRAY_BUFFER, new Float32Array(cast vertices), GL.STATIC_DRAW);
     GL.bindBuffer(GL.ARRAY_BUFFER, null);
 
     var textureCoords = [
       // Front face
       0.0, 0.0,
-      1.0, 0.0,
-      1.0, 1.0,
       0.0, 1.0,
+      1.0, 1.0,
+      1.0, 0.0,
 
       // Back face
       1.0, 0.0,
-      1.0, 1.0,
-      0.0, 1.0,
       0.0, 0.0,
+      0.0, 1.0,
+      1.0, 1.0,
 
       // Top face
       0.0, 1.0,
-      0.0, 0.0,
-      1.0, 0.0,
       1.0, 1.0,
+      1.0, 0.0,
+      0.0, 0.0,
 
       // Bottom face
       1.0, 1.0,
-      0.0, 1.0,
-      0.0, 0.0,
       1.0, 0.0,
+      0.0, 0.0,
+      0.0, 1.0,
 
       // Right face
       1.0, 0.0,
-      1.0, 1.0,
-      0.0, 1.0,
       0.0, 0.0,
+      0.0, 1.0,
+      1.0, 1.0,
 
       // Left face
       0.0, 0.0,
-      1.0, 0.0,
-      1.0, 1.0,
       0.0, 1.0,
+      1.0, 1.0,
+      1.0, 0.0
     ];
     cubeVertexTextureCoordBuffer = GL.createBuffer();
     GL.bindBuffer(GL.ARRAY_BUFFER, cubeVertexTextureCoordBuffer);
     GL.bufferData(GL.ARRAY_BUFFER, new Float32Array(cast textureCoords), GL.STATIC_DRAW);
 
     var cubeVertexIndices = [
-        0, 1, 2,      0, 2, 3,    // Front face
-        4, 5, 6,      4, 6, 7,    // Back face
-        8, 9, 10,     8, 10, 11,  // Top face
-        12, 13, 14,   12, 14, 15, // Bottom face
-        16, 17, 18,   16, 18, 19, // Right face
-        20, 21, 22,   20, 22, 23  // Left face
+      0, 1, 2,      0, 2, 3,    // Front face
+      4, 5, 6,      4, 6, 7,    // Back face
+      8, 9, 10,     8, 10, 11,  // Top face
+      12, 13, 14,   12, 14, 15, // Bottom face
+      16, 17, 18,   16, 18, 19, // Right face
+      20, 21, 22,   20, 22, 23  // Left face
     ];
     cubeVertexIndexBuffer = GL.createBuffer();
     GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
-    GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Int16Array(cubeVertexIndices), GL.STATIC_DRAW);
+    GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Int16Array(cast cubeVertexIndices), GL.STATIC_DRAW);
   }
 
   /**
    *
    */
-   private function setMatrixUniforms() {
-    GL.uniformMatrix3D( pMatrixUniform,  false, pMatrix  );
+  private function setMatrixUniforms():Void {
+    GL.uniformMatrix3D( pMatrixUniform, false, pMatrix );
     GL.uniformMatrix3D( mvMatrixUniform, false, mvMatrix );
-  }
+  } 
 
   /**
    *
    */
-   private function renderView(rect:Rectangle):Void {
+  private function renderView(rect:Rectangle):Void {
     update();
-
     GL.viewport(Std.int(rect.x), Std.int(rect.y), Std.int(rect.width), Std.int(rect.height));
 
     GL.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -281,7 +276,7 @@ class Main extends Sprite {
     GL.vertexAttribPointer(vertexPositionAttribute, 3, GL.FLOAT, false, 0, 0);
 
     GL.bindBuffer(GL.ARRAY_BUFFER, cubeVertexTextureCoordBuffer);
-    GL.vertexAttribPointer(textureCoordAttribute, 2, GL.FLOAT, false, 0, 0);
+    GL.vertexAttribPointer(texCoordAttribute, 2, GL.FLOAT, false, 0, 0);
 
     GL.activeTexture(GL.TEXTURE0);
     GL.bindTexture(GL.TEXTURE_2D, texture);
@@ -301,7 +296,7 @@ class Main extends Sprite {
   /**
    *
    */
-  private function update() {
+  private function update():Void {
     var timeNow = Lib.getTimer();
 
     if (lastTime != 0) {
@@ -317,23 +312,7 @@ class Main extends Sprite {
   /**
    *
    */
-  private function mvPushMatrix() {
-    mvMatrixStack.push( mvMatrix.clone() );
-  }
-
-  /**
-   *
-   */
-  private function mvPopMatrix() {
-    if (mvMatrixStack.length > 0) {
-      mvMatrix = mvMatrixStack.pop();
-    }
-  }
-
-  /**
-   *
-   */
-  public static function perspective(fovy:Float, aspect:Float, near:Float, far:Float) {
+  public static function perspective(fovy:Float, aspect:Float, near:Float, far:Float):Matrix3D {
     var top = near*Math.tan(fovy*Math.PI/360.0);
     var bottom = -top;
     
@@ -357,7 +336,7 @@ class Main extends Sprite {
   /**
    *
    */
-  private function rearrangeColorComponents(values:UInt8Array, w:Int, h:Int) {
+  private function rearrangeColorComponents(values:UInt8Array, w:Int, h:Int):UInt8Array {
     var result = new UInt8Array(w*h*4);
 
     for (i in 0...w*h) {
@@ -370,5 +349,20 @@ class Main extends Sprite {
     return result;
   }
 
+  /**
+   *
+   */
+  private function mvPushMatrix():Void {
+    mvMatrixStack.push( mvMatrix.clone() );
+  }
+
+  /**
+   *
+   */
+  private function mvPopMatrix():Void {
+    if (mvMatrixStack.length > 0) {
+      mvMatrix = mvMatrixStack.pop();
+    }
+  }
 
 }
